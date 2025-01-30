90912880e6578e4dbb65060bb0c7b7860bcb1889
Yokkoo <yokko.pro@outlook.fr>
2025-01-30 11:47:50 +0100

    - Mise en place des fichiers principaux

---
+++ Site Web/index.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'sql.php';
+
+// Fermeture de la connexion
+$mysqlClient = null;
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Les Recettes de Grand mère</title>
+</head>
+<body>
+    <p>Page Recette de grand mère ou jsp</p>
+    <a href=""></a>
+</body>
+</html>
